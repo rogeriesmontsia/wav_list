3bb60fc489691f67e90ef457cde26ea9cf5eec00
XaviFibla <xavi.fibla@gmail.com>
2023-12-18 13:04:16 +0100

    React strcutre of page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom';
+import {  Discover } from './pages'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="relative flex">
+      Sidebar
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+      SearchBar
+
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            <Routes>
+              <Route path="/" element={<Discover />} />
+            </Routes>
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">
+          TopPlay
+          </div>
+        </div>
       </div>
-      <h1>Vite + React HOLSA</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
