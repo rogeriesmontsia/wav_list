3191360de192d4d301f77ff8517c41ede3421e3d
XaviFibla <xavi.fibla@gmail.com>
2024-01-16 18:03:21 +0100

    Update backend code and requirements

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,5 @@
 API
+
+
+Debug FastAPI with Docker and Visual Studio Code
+https://medium.com/@nhduy88/setup-debugger-for-your-fastapi-project-with-vscode-and-docker-compose-bc4f61702b69
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,19 @@
-from fastapi import FastAPI,Depends
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import Response
+from typing import Union
+import uvicorn
+from fastapi import FastAPI
 
 app = FastAPI()
-origins = [
-    "http://localhost:5173",
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-    
-@app.get("/",status_code=200) 
-def home():
-    test = '{"name":"Johnss", "age":8, "car":null}'
-    return Response(content=test, media_type="application/json")
+
+
+@app.get("/products")
+def read_root():
+    '''TODO petició api X'''
+    return {"producte1": "asdfasdfsadfdsaf"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "qss": q}
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",host="127.0.0.1",port=8181,reload=True)
