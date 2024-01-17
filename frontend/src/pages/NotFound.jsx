dde18f965ca5afcec8b61a7834cb992bcb20ccd0
Macarena Gonzales <144931898+macarenagonzales@users.noreply.github.com>
2024-01-17 21:01:21 +0100

    redireccion

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const NotFound = () => {
+        const redirectToDashboard = () => {
+            //Ruta al Dashboard
+            console.log('Redireccionando al Dashboard...');
+            window.location.href = '/dashboard';
+        };
+
     return (
         <div class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
             <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
@@ -12,7 +18,7 @@ const NotFound = () => {
                                 doorway to the great nothing
                             </h1>
                             <p class="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
-                            <button class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Take me there!</button>
+                            <button class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50" onClick={redirectToDashboard}>Take me there!</button>
                         </div>
                     </div>
                     <div>
