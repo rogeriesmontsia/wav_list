97df5b97a0c12ec352b4c578fc261bc29a2a8094
XaviFibla <xavi.fibla@gmail.com>
2024-02-02 19:35:27 +0100

    Add login token endpoint and authentication logic

--- backend/app/controllers/login.py
+++ backend/app/controllers/login.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter, Depends, HTTPException, Security, Form
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from app.services.login import LoginService
 from app.models.requests.login import LoginReq,UserCreate
+from app.security.secure import authenticate, create_access_token
+
 import re
+from datetime import datetime, timedelta
 router = APIRouter()
 
 pattern_email =  r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
@@ -25,4 +28,28 @@ def login_app(user:UserCreate, loginservice:LoginService = Depends(LoginService)
     else:
         return JSONResponse(content={'message':'email is not valid'}, status_code=401)
     return jsonable_encoder(login)
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+@router.post("/login/token")
+def access_token(user:LoginReq, loginservice:LoginService = Depends(LoginService)):
     
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    account = loginservice.get_login(user.username)
+    if authenticate(user.username, user.password, account):
+        access_token = create_access_token(
+            data={"sub": user.username, "scopes": "admin_read admin_write user guest"},
+            expires_delta=access_token_expires,
+        )
+    
+        return {
+            "access_token": access_token,
+            "expires_in": access_token_expires,
+            "token_type": "Bearer",
+            "userid": user.username,
+            "scope": "SCOPE"
+           
+        }
+    else:
+        raise HTTPException(
+            status_code=400, detail="Incorrect credentials")
--- backend/app/security/secure.py
+++ backend/app/security/secure.py
@@ -4,6 +4,7 @@ from app.utils import config
 from datetime import datetime, timedelta
 from fastapi.security import OAuth2AuthorizationCodeBearer, SecurityScopes 
 from passlib.context import CryptContext
+from app.models.data.sqlalchemy_models import Users
 
 SECRET_KEY = config.SECRET_KEY
 ALGORITHM = "HS256"
@@ -19,12 +20,21 @@ oauth2_scheme = OAuth2AuthorizationCodeBearer(
              "user":"valid user of the application",
              "guest":"visitor of the site" },
     )
+def verify_password(plain_password, hashed_password):
+    return crypt_context.verify(plain_password, hashed_password)
 
-def create_access_token(data: dict, expires_after: timedelta):
-    plain_text = data.copy()
-    expire = datetime.utcnow() + expires_after
-    plain_text.update({"exp": expire})
-    encoded_jwt = jwt.encode(plain_text, SECRET_KEY, algorithm=ALGORITHM)
+def authenticate(username, password, account:Users):
+    try:
+        password_check = verify_password(password, account.password)
+        return password_check
+    except Exception as e:
+        print(e)
+        return False
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 def get_password_hash(password):
